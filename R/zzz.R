203cdae094513f3ebfa8b5b87ea2339270e6351b
Ivan Svetunkov <ivan@svetunkov.ru>
2022-11-28 17:40:59 +0000

    Greeting for the package

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onAttach <- function(libname, pkgname) {
+    startUpMessage <- paste0("Package \"complex\", v",packageVersion(pkgname)," loaded.");
+    packageStartupMessage(startUpMessage);
+}
+
+.onUnload <- function (libpath) {
+  library.dynam.unload("complex", libpath);
+}
